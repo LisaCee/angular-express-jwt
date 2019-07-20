ca4ab769eff4fbc8d4757e31671318be0339e993
LisaCee <lisaceedesign@gmail.com>
2019-07-20 14:02:18 -0700

    displaying data using material through page 72

--- frontend/src/app/contact-list/contact-list.component.ts
+++ frontend/src/app/contact-list/contact-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Contact } from '../contact';
 
 @Component({
   selector: 'app-contact-list',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent implements OnInit {
-
-  constructor() { }
+  displayedColumns: string[] = ['name', 'email', 'country', 'title', 'actions'];
+  dataSource = [];
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+    this.apiService.getContacts().subscribe((res) => {
+      console.log('BODY', res.body)
+      this.dataSource = res.body
+    })
   }
-
+  previousPage() {
+    this.apiService.getContacts(this.apiService.prev).subscribe((res) => {
+      console.log(res.body);
+      this.dataSource = res.body;
+    });
+  }
+  nextPage() {
+    this.apiService.getContacts(this.apiService.next).subscribe((res) => {
+      console.log(res.body);
+      this.dataSource = res.body;
+    });
+  }
+  firstPage() {
+    this.apiService.getContacts(this.apiService.first).subscribe((res) => {
+      console.log(res.body);
+      this.dataSource = res.body;
+    });
+  }
+  lastPage() {
+    this.apiService.getContacts(this.apiService.last).subscribe((res) => {
+      console.log(res.body);
+      this.dataSource = res.body;
+    });
+  }
 }
